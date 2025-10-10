74b181f6802b4d7b8a081d2015c636e69c84a395
xuanphuda <xuanphuda@gmail.com>
2025-10-10 21:39:27 +0700

    feat: add login with Google

--- SoTroOnline/QuanLyTaiKhoan/src/main/java/com/so_tro_online/quan_ly_tai_khoan/service/EmailService.java
+++ SoTroOnline/QuanLyTaiKhoan/src/main/java/com/so_tro_online/quan_ly_tai_khoan/service/EmailService.java
@@ -1,7 +1,5 @@
 package com.so_tro_online.quan_ly_tai_khoan.service;
 
-import com.so_tro_online.quan_ly_tai_khoan.exception.EmailSendFailedException;
-import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
